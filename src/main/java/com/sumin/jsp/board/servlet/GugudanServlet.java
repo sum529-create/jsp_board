eb8d402812d277c238f7f5041788a40f41bf73e3
sum529-create <nosumin29@gmail.com>
2024-11-10 15:15:06 +0900

    setting servelet

---
+++ src/main/java/com/sumin/jsp/board/servlet/GugudanServlet.java
@@ -0,0 +1,23 @@
+package com.sumin.jsp.board.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/gugudan")
+public class GugudanServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html; charset=utf-8");
+
+        int dan = 8;
+        int i = 1;
+        
+        resp.getWriter().append("안녕!ㅁㅁ");
+    }
+}
---
+++ src/main/java/com/sumin/jsp/board/servlet/HomeServlet.java
@@ -0,0 +1,20 @@
+package com.sumin.jsp.board.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html; charset=utf-8");
+        resp.getWriter().append("안녕!ㅁㅁ");
+    }
+}
